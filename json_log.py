7579e7a631371186c9dedee43e07b1e27bc0cf89
Benjamin Shaya <bshaya@restdevices.com>
2016-01-27 11:22:30 -0500

    Working v1!

---
+++ json_log.py
@@ -0,0 +1,81 @@
+'''
+json_log.py
+
+A dead simple module that reads a file and converts json objects to array elements
+Does bracket and quote matching to handle nested objects
+'''
+
+import json
+
+# Read the entirety of a file as dictionaries
+def read(filename):
+    file = open(filename)
+    contents = [readItem(file)]
+    while contents[-1] != None:
+        contents.append(readItem(file))
+    contents.pop()
+    return contents
+
+# Read the next json item from the file
+def readItem(file):
+    # Read until next '{'
+    nextChar = 0
+    while nextChar != '{' and nextChar != '':
+        nextChar = file.read(1)
+
+    if nextChar == '':
+        return None
+    # Store until complete
+    objectBuilder = ObjectBuilder()
+    objectBuilder.addChar(nextChar)
+    while not objectBuilder.isComplete() and nextChar != '':
+        nextChar = file.read(1)
+        objectBuilder.addChar(nextChar)
+
+    if not objectBuilder.isComplete():
+        return None
+    # Parse JSON Object
+    print objectBuilder.getObject()
+    return json.loads(objectBuilder.getObject())
+
+# Helper class to keep track of characters that effect where the object ends
+class ObjectBuilder:
+    def __init__(self):
+        self.openBrackets = 0
+        self.inQuotes = False
+        self.escaped = False
+        self.object = ""
+        
+    # Add a character and keep track of opened brackets
+    # (To do so, must be quote-aware, and to do that, must be escape-aware)
+    # (This got way more complicated than I hoped)
+    def addChar(self, char):
+        if self.escaped:
+            self.escaped = False
+        elif char == '{':
+            if not self.inQuotes:
+                self.openBrackets += 1
+        elif char == '}':
+            if not self.inQuotes:
+                self.openBrackets -= 1
+        elif char == '\\':
+            if self.inQuotes:
+                self.escaped = True
+        elif char == '"':
+            self.inQuotes = not self.inQuotes
+        self.object += char
+            
+    def isComplete(self):
+        return self.openBrackets == 0
+        
+    def getObject(self):
+        return self.object
+
+if __name__ == '__main__':
+    import sys
+    testFile = "test.json"
+    if len(sys.argv) > 1:
+        testFile = sys.argv[1]
+
+    print "Loading " + testFile + ":"
+    print read(testFile)
